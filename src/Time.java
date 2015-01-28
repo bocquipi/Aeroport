bec31f2a22717610c7800985fd1cfd37f97bdd07
bocquipi <bocquipi@d1-204-14.eleve.interne.enac>
2015-01-28 15:13:55 +0100

    Faire Timer

--- src/Time.java
+++ src/Time.java
@@ -1,4 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Observable;
 
-public class Time {
+import javax.swing.Timer;
+
+/* Projet JAVA         */
+/* Modele              */
+/* author :            */
+
+public class Time extends Observable implements ActionListener {
+
+	/** Declaration des variables privees **/
+	private Timer timer;
+	private int delais;
+	
+	/** Constructeur de la classe Echelle **/
+	public Time(Fenetre fenetre) {
+		
+		delais = 5000; //Par defaut : vitesse = 5ms
+		create_timer();
+	}
+
+	/** create_timer **/
+	/** fonction : creation d'un timer **/
+	public void create_timer() {
+		timer = new Timer(delais, this);
+		timer.setInitialDelay(delais);
+		timer.start(); 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
